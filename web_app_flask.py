1ac3174039e55ec651bfaf26314d4d743913925d
Maksym Mykhasiuta <maksym.mykhasiuta@ucu.edu.ua>
2022-02-19 00:26:19 +0200

    minor changes

--- web_app_flask.py
+++ web_app_flask.py
@@ -45,8 +45,6 @@ def sign_up():
     return render_template("map.html")
 
 
-app.run(host='localhost', port=5000)
-
 # def index():
 #     start_coords = (46.9540700, 142.7360300)
 #     folium_map = folium.Map(location=start_coords, zoom_start=14)
@@ -54,4 +52,4 @@ app.run(host='localhost', port=5000)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='localhost', port=5000)
